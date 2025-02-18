29c00fa16ba41fd46c4575748c1b4f6f44ee5501
Michael Oluwakayode <michaelaykay@gmail.com>
2025-02-18 06:54:42 -0800

    feat: tweaking of AI system instruction

--- backend/controllers/aichat.controller.js
+++ backend/controllers/aichat.controller.js
@@ -5,10 +5,12 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 const chatGemini = async (req, res) => {
     const userInput = req.body.prompt;
+    const systemInstructions = "Your name is iLife AI. Always refer to yourself as iLife AI when introducing yourself or responding to users.";
+
     try {
         const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-        const response = await model.generateContent([userInput]);
+        const response = await model.generateContent([systemInstructions, userInput]);
 
         if (!response || !response.response || !response.response.candidates || response.response.candidates.length === 0) {
             throw new Error("Invalid response structure");
@@ -19,7 +21,7 @@ const chatGemini = async (req, res) => {
         res.json({ response: aiMessage });
     } catch (error) {
         console.error("err generating response", error.aiMessage?.data || error.message);
-        res.status(500).send("An err occured while generating the response");
+        res.status(500).send("An error occurred while generating the response");
     }
 };
 
